9aef3850754871e0b1c4a3990e989356f5a7c490
jimhome1608@gmail.com <jimhome1608@gmail.com>
2016-11-01 13:16:44 +1100

    Got percentages showing headings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,12 +19,32 @@ namespace Multilink2.Controllers
 
         public ActionResult vwInternetEnquiries(String SalesMethod)
         {
+            List<WebEnquires> _list;
             if ((Session["TypedListModel"] == null) || (Session["SalesMethod"] != SalesMethod))
-               Session["TypedListModel"] = WebEnquires.GetList(SalesMethod);
+            {
+                _list = WebEnquires.GetList(SalesMethod);
+                float _total_count = 0;
+                float _rea_count = 0;
+                float _dca_count = 0;
+                float _rvw_count = 0;
+                foreach (WebEnquires _WebEnquires in _list)
+                {
+                    _total_count = _total_count + _WebEnquires.total_count;
+                    _rea_count = _rea_count + _WebEnquires.rea_count;
+                    _dca_count = _dca_count + _WebEnquires.dca_count;
+                    _rvw_count = _rvw_count + _WebEnquires.rvw_count;
+                }
+                ViewBag.EnquiresCaption = "Enquiries<br /> <span class='percent_used'> " + String.Format("Total: {0:}", _total_count);
+                ViewBag.ReaCaption = "REA<br /> <span class='percent_used'> " + String.Format("{0:0.0}%", _rea_count/_total_count*100);
+                ViewBag.DcaCaption = "DCA<br /> <span class='percent_used'> " + String.Format("{0:0.0}%", _dca_count / _total_count * 100);
+                ViewBag.RvwCaption = "RVW<br /> <span class='percent_used'> " + String.Format("{0:0.0}% </span>", _rvw_count / _total_count * 100);
+                Session["TypedListModel"] = _list;
+            }
+               
             Session["SalesMethod"] = SalesMethod;
             SalesMethod = SalesMethod.Replace("and", "&");
             ViewBag.UserLocation = "Web Enquiries";
-            ViewBag.UserLocation2 = SalesMethod;
+            ViewBag.UserLocation2 = SalesMethod;            
             ViewBag.OfficeName = "Noel Jones - Box Hill";
             return View(Session["TypedListModel"]);
         }
