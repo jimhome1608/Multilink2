ddb9bf1e202ef813bc6230f09cb110cea1117ed0
jimhome1608@gmail.com <jimhome1608@gmail.com>
2016-11-19 21:50:54 +1100

    Added Inspections

---
+++ Controllers/InspectionsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multilink2.Controllers
+{
+    public class InspectionsController : Controller
+    {
+        //
+        // GET: /Inspections/
+
+        public ActionResult Inspections()
+        {
+            ViewBag.UserLocation = "Inspection";
+            ViewBag.UserLocation2 = "Sat. 19/11/2016";
+            ViewBag.BaseLocation = "8-18 Glass St";
+            return View();
+        }
+
+    }
+}
