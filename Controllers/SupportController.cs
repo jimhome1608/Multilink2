583053e9bb526d32fd31402bf1654d2d6bd88f74
jimhome1608@gmail.com <jimhome1608@gmail.com>
2016-12-15 23:22:46 +1100

    New Multilink Support menu when user_office_id = 1 is coming along

---
+++ Controllers/SupportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multilink2.Controllers
+{
+    public class SupportController : Controller
+    {
+        //
+        // GET: /Support/
+
+        public ActionResult vwFindAgent()
+        {
+            ViewBag.UserLocation = "Find Agent";
+            ViewBag.BaseLocation = "Support";
+            return View();
+        }
+
+    }
+}
