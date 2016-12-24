97a424d97d4122893d2e01a5d1dcab1b1cc0e8e2
jimhome1608@gmail.com <jimhome1608@gmail.com>
2016-12-24 21:11:59 +1100

    Added find user

--- Controllers/SupportController.cs
+++ Controllers/SupportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Multilink2.Models;
+using System.Data.Odbc;
 
 namespace Multilink2.Controllers
 {
@@ -20,6 +21,70 @@ namespace Multilink2.Controllers
             ViewBag.BaseLocation = "Multilink Support";
             return View(_list);
         }
+
+        public ActionResult FindUser()
+        {
+            ViewBag.UserLocation = "Find User";
+            ViewBag.BaseLocation = "Multilink Support";
+            var model = new AgentInfoList();
+            model = readAgentInfo("");
+            return View("vwFindUser", model);
+        }
+
+
+        [HttpPost]
+        public ActionResult RefreshAgent(AgentInfoList model)
+        {
+            ViewBag.UserLocation = "Find User";
+            ViewBag.BaseLocation = "Multilink Support";
+            model = readAgentInfo(model.SearchString);
+            return View("vwFindUser", model);
+        }
+
+        private String abbreviateOfficeNames(String _officeName)
+        {
+            _officeName.Trim();
+            _officeName = _officeName.Replace("Noel Jones", "N.Jones:");
+            _officeName = _officeName.Replace("Barry Plant", "B.Plant:");
+            _officeName = _officeName.Replace("Real Estate", " ");
+            _officeName = _officeName.Replace("Management", " ");
+            _officeName = _officeName.Replace("Pty Ltd", " ");
+            _officeName = _officeName.Replace("Development", " ");
+            _officeName = _officeName.Replace("Waverley City First National", "1st Nat.: Glen Waverley");
+            return _officeName;
+
+        }
+
+        public AgentInfoList readAgentInfo(String _SearchString)
+        {
+            var model = new AgentInfoList();
+            model.SearchString = _SearchString;
+            DB db = new DB();
+            db.open();
+            using (OdbcCommand query = new OdbcCommand("", db.odbcConnection))
+            {
+                query.CommandText = "exec findPerson2 '" + _SearchString + "'";
+                OdbcDataReader reader = query.ExecuteReader();
+                while (reader.Read() == true)
+                {
+
+                    AgentInfo newRecord = new AgentInfo();
+                    newRecord.agent_name = reader["agent_name"].ToString();
+                    newRecord.agent_name = abbreviateOfficeNames(newRecord.agent_name);
+                    newRecord.agent_suburb = reader["agent_suburb"].ToString();
+                    newRecord.agent_phone = reader["agent_phone"].ToString();
+                    newRecord.full_name = reader["full_name"].ToString();
+                    newRecord.user_mobile = reader["user_mobile"].ToString();
+                    newRecord.user_phone_bh = reader["user_phone_bh"].ToString();
+                    newRecord.user_email = reader["user_email"].ToString();
+                    newRecord.ya_password = reader["ya_password"].ToString();
+                    model.items.Add(newRecord);
+                }
+            }
+            db.close();
+            return model;
+        }
+
 
     }
 }
---
+++ Models/FindUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multilink2.Models
+{
+
+
+    public class AgentInfoList
+    {
+        public string SearchString { get; set; }
+
+        public List<AgentInfo> items { get; set; }
+
+        public AgentInfoList()
+        {
+            this.items = new List<AgentInfo>();
+        }
+
+    }
+
+
+    public class AgentInfo
+    {
+
+        public string agent_name { get; set; }
+        public string agent_suburb { get; set; }
+        public string agent_phone { get; set; }
+        public string full_name { get; set; }
+        public string user_mobile { get; set; }
+        public string user_phone_bh { get; set; }
+        public string user_email { get; set; }
+        public string ya_password { get; set; }
+
+    }
+}
