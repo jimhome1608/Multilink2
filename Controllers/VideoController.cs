361de1c07656fb640cce70ab5a211d67d8bae85c
jimhome1608@gmail.com <jimhome1608@gmail.com>
2017-01-15 16:27:23 +1100

    Added video for Weblink  update

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multilink2.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult WeblinkUpdate()
+        {
+            ViewBag.UserLocation = "Weblink Update Jan 2017";
+            ViewBag.BaseLocation = "Video";
+            return View();
+        }
+
+    }
+}
